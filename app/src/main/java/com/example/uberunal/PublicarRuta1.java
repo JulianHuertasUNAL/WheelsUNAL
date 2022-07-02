857900161add446fafa301eb221b99464b3cf3c0
daherrera01 <daherreran@unal.edu.co>
2022-07-02 18:01:56 -0500

    Se reciben los datos

--- app/src/main/java/com/example/uberunal/PublicarRuta1.java
+++ app/src/main/java/com/example/uberunal/PublicarRuta1.java
@@ -5,8 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class PublicarRuta1 extends AppCompatActivity {
+    EditText DireccionOrigen;
+    EditText Dias;
+    EditText Salida;
+    EditText CantidadCupos;
+    EditText Celular;
 
     public void publicarruta(View view){
         Intent i = new Intent(this, TipoRuta.class);
@@ -18,5 +25,26 @@ public class PublicarRuta1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_publicar_ruta1);
+        DireccionOrigen = findViewById(R.id.destino);
+        Dias = findViewById(R.id.dias);
+        Salida = findViewById((R.id.hora));
+        CantidadCupos = findViewById(R.id.cantidadcupos);
+        Celular = findViewById(R.id.contacto);
+    }
+    public void BotonGuardar(View view){
+        String Origen = DireccionOrigen.getText().toString();
+        String Dia = Dias.getText().toString();
+        String HoraSalida = Salida.getText().toString();
+        String NumCupos = CantidadCupos.getText().toString();
+        int Cupos = Integer.parseInt(NumCupos);
+
+        String Contacto = Celular.getText().toString();
+
+        if(Origen.isEmpty() || Dia.isEmpty() || HoraSalida.isEmpty() || NumCupos.isEmpty() || Contacto.isEmpty()){
+            Toast.makeText(this,"Ingrese todos los datos",Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(this, "Datos guardados correctamente", Toast.LENGTH_SHORT).show();
+        }
     }
 }
